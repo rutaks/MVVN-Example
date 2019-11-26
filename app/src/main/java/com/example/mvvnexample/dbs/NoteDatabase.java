d7bfe3313fd7f203d596861ab92aa789c6ee3fcc
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-26 21:35:54 +0200

    Added Concurrency control & instanciate noteDao

--- app/src/main/java/com/example/mvvnexample/dbs/NoteDatabase.java
+++ app/src/main/java/com/example/mvvnexample/dbs/NoteDatabase.java
@@ -1,11 +1,18 @@
 package com.example.mvvnexample.dbs;
 
 import android.content.Context;
+
+import com.example.mvvnexample.dao.NoteDao;
+import com.example.mvvnexample.models.Note;
+
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
     private static NoteDatabase instance;
+    public abstract NoteDao noteDao();
 
     public static synchronized NoteDatabase getInstance(Context context){
         if (instance == null) {
