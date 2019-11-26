decefb0930131cc153a83cc07c4d07a013162851
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-26 21:36:16 +0200

    Setup repository

--- app/src/main/java/com/example/mvvnexample/repo/NoteRepo.java
+++ app/src/main/java/com/example/mvvnexample/repo/NoteRepo.java
@@ -1,4 +1,41 @@
 package com.example.mvvnexample.repo;
 
+import android.app.Application;
+
+import com.example.mvvnexample.dao.NoteDao;
+import com.example.mvvnexample.dbs.NoteDatabase;
+import com.example.mvvnexample.models.Note;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
 public class NoteRepo {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> notes;
+
+    public NoteRepo(Application application) {
+        NoteDatabase db = NoteDatabase.getInstance(application);
+        noteDao = db.noteDao();
+        notes = noteDao.getAll();
+    }
+
+    public void insert(Note note){
+
+    }
+
+    public void update(Note note){
+
+    }
+    public void delete(Note note){
+
+    }
+
+    public void deleteAll(){
+
+    }
+
+    public LiveData<List<Note>> getAll(){
+        return notes;
+    }
 }
