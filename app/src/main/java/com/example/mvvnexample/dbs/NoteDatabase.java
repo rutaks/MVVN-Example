ceb4980e62d7c5d3b25b28ef4463ed2931819578
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-26 21:52:03 +0200

    Added Callback to reload live data notes

--- app/src/main/java/com/example/mvvnexample/dbs/NoteDatabase.java
+++ app/src/main/java/com/example/mvvnexample/dbs/NoteDatabase.java
@@ -1,13 +1,16 @@
 package com.example.mvvnexample.dbs;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import com.example.mvvnexample.dao.NoteDao;
 import com.example.mvvnexample.models.Note;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -23,5 +26,28 @@ public abstract class NoteDatabase extends RoomDatabase {
                    .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback(){
+        public void onCreate(@NonNull SupportSQLiteDatabase db){
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title", "Description", 1));
+            noteDao.insert(new Note("Title", "Description", 1));
+            noteDao.insert(new Note("Title", "Description", 1));
+            return null;
+        }
     }
 }
