b64b1f86a45dd99a259ad9a75d1d6fa078ee447a
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-27 18:11:28 +0200

    Setup Note Adapter

---
+++ app/src/main/java/com/example/mvvnexample/adapter/NoteAdapter.java
@@ -0,0 +1,57 @@
+package com.example.mvvnexample.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mvvnexample.R;
+import com.example.mvvnexample.models.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(View view) {
+            super(view);
+
+            textViewTitle = view.findViewById(R.id.text_view_title);
+            textViewDescription = view.findViewById(R.id.text_view_description);
+            textViewPriority = view.findViewById(R.id.text_view_priority);
+        }
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.textViewTitle.setText(note.getTitle());
+        holder.textViewDescription.setText(note.getDescription());
+        holder.textViewPriority.setText(note.getPriority());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes){
+        this.notes = notes;
+    }
+}
