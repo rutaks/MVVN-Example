c413bbf99fe96841633bf9b453d925baed5dc645
Rutakayile Samuel <samrutakayile@awesomity.rw>
2019-11-27 19:16:42 +0200

    Set Main Activity To Receive Data On Result Retreived

--- app/src/main/java/com/example/mvvnexample/activities/MainActivity.java
+++ app/src/main/java/com/example/mvvnexample/activities/MainActivity.java
@@ -1,21 +1,27 @@
-package com.example.mvvnexample;
+package com.example.mvvnexample.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.example.mvvnexample.R;
 import com.example.mvvnexample.adapter.NoteAdapter;
 import com.example.mvvnexample.models.Note;
 import com.example.mvvnexample.viewmodel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int  ADD_NOTE_REQUEST = 1;
     private NoteViewModel noteViewModel;
 
     @Override
@@ -37,5 +43,31 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);
             }
         });
+
+        FloatingActionButton addNoteButton = findViewById(R.id.button_add_note);
+        addNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK){
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title,description,priority);
+            noteViewModel.insert(note);
+            Toast.makeText(this, "New Note Added", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "New Note Not Added", Toast.LENGTH_SHORT).show();
+        }
     }
 }
